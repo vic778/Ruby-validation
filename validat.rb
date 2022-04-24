a94129f1b9037e49e856afb450f2a8e371b0e791
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-24 21:32:25 +0300

    Add vali

--- validat.rb
+++ validat.rb
@@ -1,20 +1,49 @@
 module Validatable
 
-    @@validations={}
-    # I need receive = {presence: [:weight, :length], aircraft_type: [:length]}
-
-    def self.validates_presence_of(*attrs)
-        @@validations[:presence] = attrs
-    end
-
-    def validate 
-        @@validations.each do |v, fields| 
-            fields.each {|field_name| self.send("validate_#{v}_of", field_name)}
+   def validate_presence_of(*attributes)
+        attributes.each do |attribute|
+            puts "validating presence of #{attribute}"
+             
         end
     end
 
     private 
 
-    def validate_presence_of(field_name)
+    def validate_presence_of(name, number)
+        if name.nil? || number.nil?
+            errors.add(:name, "can't be blank")
+            errors.add(:number, "can't be blank")
+        end
+    end
+
+end
+
+class User
+    include Validatable
+    
+    attr_accessor :name, :number
+    
+    def initialize(name, number)
+        @name = name
+        @number = number
+        
+        # @number = options[:number].to_i
+
+    end
+
+    def validate
+        validate_presence_of(name, number)
+    end
+
+    
+    def valid?
+        true
     end
 end
+
+
+c = User.new(name = "vic", number = "123")
+puts c.name
+puts c.number
+puts c.validate
+puts c.valid?
